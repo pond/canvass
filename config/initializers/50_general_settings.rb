f7d98c7177429809d86ceef2964f3d82a96faf8c
rool <rool@fbfdfdf0-2b19-0410-bbe2-c4a1547f2368>
2011-06-03 10:26:06 +0000

    Simple modification to Canvass to let it read from a plain text file and report the first line of this as an amount left in a donation matching "pot" in the list of bounties.

--- config/initializers/50_general_settings.rb
+++ config/initializers/50_general_settings.rb
@@ -22,3 +22,8 @@ ADMINISTRATOR_EMAIL_ADDRESS = NOTIFICATION_EMAILS_COME_FROM
 # Maximum number of items to show per page in sortable list views.
 
 MAXIMUM_LIST_ITEMS_PER_PAGE = 25
+
+# ROOL specific change.
+
+MATCHING_POT_CURRENCY = 'GBP'
+MATCHING_POT_LOCATION = "/home/rool/shared/matching_pot.txt"
